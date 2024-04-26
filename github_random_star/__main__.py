700427a7ff56f87cd080a714ab16769f72801eca
David Kasakaitis <davidkasakaitis@proton.me>
2024-04-26 15:53:53 +0100

    refactor: api access
    Moved api access and cache handling to seperate module/class.

--- github_random_star/__main__.py
+++ github_random_star/__main__.py
@@ -3,14 +3,14 @@ import logging as log
 import os
 import random
 import subprocess
-from datetime import datetime
 from pathlib import Path
-from typing import NamedTuple, Optional
+from typing import Optional
 
 import fire
-import httpx
 
-USER_API_URL = "https://api.github.com/users/{user}/starred?page={page}&per_page=30"
+
+from github_random_star.api import StarredItem, GithubAPI
+
 
 if os.getenv("PYTEST_TESTING"):
     CACHE_PATH = Path("tests/files")
@@ -36,99 +36,6 @@ IGNORE_FILE = CACHE_PATH / Path("ignore.json")
 
 class AccountMissingError(TypeError):
     "GitHub account was not provided through flags or an environment variable."
-
-
-class StarredItem(NamedTuple):
-    repo_id: int
-    name: str
-    url: str
-
-
-def retrieve_cache(
-    account: str,
-    *,
-    refresh: bool = False,
-    max_results: Optional[int] = None,
-) -> set[StarredItem]:
-    """Retrieves data for choosing a random starred item from GitHub.
-
-    Args:
-        account: Github account the starred items will be retrieved from.
-        refresh: Whether or not to refresh the cache. Defaults to False.
-        max_results: Maximum amount of items to retrieve. If left as None all
-            items will be retrieved.
-
-    Raises:
-        ConnectionError: If the outgoing request was not successful.
-
-    Returns:
-        set(StarredItem): Set of starred items from GitHub.
-    """
-    if CACHE_FILE.exists() and not refresh:
-        with CACHE_FILE.open("r", encoding="utf-8") as file:
-            cache_data = json.load(file)
-
-        log.info(
-            "Cache last refreshed on the %s.",
-            datetime.fromisoformat(cache_data["date"]).date().isoformat(),
-        )
-        if cache_data.get("account") == account:
-            cache_data = {StarredItem(*item) for item in cache_data["data"]}
-            # TODO: Multiple cache file for different accounts.repo:ulif/diceware
-            return cache_data
-
-        log.warning("Cache is storing a different account.")
-
-    log.info("Requesting data from Github")
-
-    headers = {"X-GitHub-Api-Version": "2022-11-28"}
-    API_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN")
-    if API_TOKEN:
-        log.info("Using provided GitHub API token")
-        headers["Authorization"] = f"Bearer {API_TOKEN}"
-    else:
-        log.warning(
-            "No GitHub API token provided. Using public access with a smaller rate limit."
-        )
-
-    data = set()
-
-    page = 1
-    while True:
-        log.info("Requesting starred items page %s", page)
-        formatted_url = USER_API_URL.format(user=account, page=page)
-        request = httpx.get(formatted_url, timeout=20, headers=headers)
-
-        if request.status_code != 200:
-            err = "Connection failed to get starred items for %s. \
-                Status Code: %s"
-            log.error(err, account, request.status_code)
-            raise ConnectionError(err % account, request.status_code)
-
-        response: list = request.json()
-        if not response:
-            break
-
-        for item in response:
-            tp = StarredItem(item["id"], item["full_name"], item["html_url"])
-            data.add(tp)
-            if max_results and len(data) >= max_results:
-                break
-
-        if max_results and len(data) >= max_results:
-            break
-
-        page += 1
-
-    with CACHE_FILE.open("w", encoding="utf-8") as file:
-        container = {
-            "account": account,
-            "date": datetime.now().isoformat(),
-            "data": tuple(data),
-        }
-        json.dump(container, file)
-
-    return data
 
 
 def extract_selection(path: Path) -> list[StarredItem]:
@@ -261,11 +168,16 @@ def main(
     if max_history is None:
         max_history = int(os.environ.get("GH_STAR_MAX_HISTORY", 100))
 
-    starred_items = retrieve_cache(
+    token = os.environ.get("GH_STAR_TOKEN")
+    github_api = GithubAPI(
         account,
+        CACHE_PATH,
         refresh=refresh,
         max_results=max_results,
+        token=token,
     )
+
+    starred_items = github_api.collect_starred_items()
 
     log.info("Total amount of starred items: %s", len(starred_items))
 
---
+++ github_random_star/api.py
@@ -0,0 +1,126 @@
+import json
+import logging
+from datetime import datetime
+from pathlib import Path
+from typing import NamedTuple, Optional
+
+import httpx
+
+log = logging.getLogger(__name__)
+
+
+class StarredItem(NamedTuple):
+    repo_id: int
+    name: str
+    url: str
+
+
+class GithubAPI:
+    USER_API_URL = "https://api.github.com/users/{user}/starred?page={page}&per_page=30"
+
+    def __init__(
+        self,
+        account: str,
+        cache_location: Path,
+        *,
+        refresh: bool = False,
+        max_results: Optional[int] = None,
+        token: Optional[str] = None,
+    ) -> None:
+        self.account = account
+        self.headers = self.create_headers(token)
+        self.cache_path = cache_location
+        self.refresh = refresh
+        self.max_results = max_results
+
+    def create_headers(self, token: Optional[str] = None) -> None:
+        headers = {"X-GitHub-Api-Version": "2022-11-28"}
+
+        if token:
+            log.info("Using provided GitHub API token")
+            headers["Authorization"] = f"Bearer {token}"
+        else:
+            log.warning(
+                "No GitHub API token provided. Using public access with a lower rate limit."
+            )
+
+        return headers
+
+    def collect_starred_items(self) -> set[StarredItem]:
+        cache = self.load_cached_items()
+        if cache:
+            return cache
+
+        log.info("Requesting data from Github")
+
+        data = set()
+
+        page = 1
+        while True:
+            response = self.get_starred_items(page)
+
+            if not response:
+                break
+
+            for item in response:
+                tp = StarredItem(
+                    item["id"],
+                    item["full_name"],
+                    item["html_url"],
+                )
+                data.add(tp)
+                if self.max_results and len(data) >= self.max_results:
+                    break
+
+            if self.max_results and len(data) >= self.max_results:
+                break
+
+            page += 1
+
+        self.save_cached_items(data)
+
+        return data
+
+    def get_starred_items(self, page: int = 1) -> list[dict]:
+        log.info("Requesting starred items page %s", page)
+        url = self.USER_API_URL.format(user=self.account, page=page)
+        response = httpx.get(url, headers=self.headers)
+        response.raise_for_status()
+
+        if response.status_code != 200:
+            err = "Connection failed to get starred items for %s. \
+                Status Code: %s"
+            log.error(err, self.account, response.status_code)
+            raise ConnectionError(err % self.account, response.status_code)
+
+        return response.json()
+
+    def load_cached_items(self) -> set[StarredItem] | None:
+        cache_path = self.cache_path / Path("cache.json")
+
+        if cache_path.exists() and not self.refresh:
+            with cache_path.open("r", encoding="utf-8") as file:
+                cache_data = json.load(file)
+
+            log.info(
+                "Cache last refreshed on the %s.",
+                datetime.fromisoformat(cache_data["date"]).date().isoformat(),
+            )
+            if cache_data.get("account") == self.account:
+                cache_data = {StarredItem(*item) for item in cache_data["data"]}
+                # TODO: Multiple cache file for different accounts.
+                return cache_data
+
+            log.warning("Cache is storing a different account.")
+
+        return None
+
+    def save_cached_items(self, data: set[StarredItem]) -> None:
+        cache_path = self.cache_path / Path("cache.json")
+        with cache_path.open("w", encoding="utf-8") as file:
+            container = {
+                "account": self.account,
+                "date": datetime.now().isoformat(),
+                "data": tuple(data),
+            }
+            json.dump(container, file)
