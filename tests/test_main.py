dbd8a2f2df7bd5bece86807e17fa349211e6db1d
David Kasakaitis <davidkasakaitis@proton.me>
2024-04-26 19:10:54 +0100

    test: ignore file

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import json
+
 import pytest
 import mock
 import builtins
@@ -23,7 +25,32 @@ def test_random_selection(set_seed, load_starred_items, cache_location):
     with mock.patch.object(builtins, "input", lambda _: 2):
         assert (
             item_selection(
-                set(load_starred_items), cache_location, total=3, max_history=-1
+                set(load_starred_items),
+                cache_location,
+                total=3,
+                max_history=-1,
             )
             is None
         )
+
+
+@pytest.mark.unit
+def test_ignore_file(set_seed, load_starred_items, cache_location):
+    ignore_file = cache_location / Path("ignore.json")
+    with ignore_file.open("r", encoding="utf-8") as file:
+        pre_len = len(json.load(file))
+
+    with mock.patch.object(builtins, "input", lambda _: 2.1):
+        assert (
+            item_selection(
+                set(load_starred_items),
+                cache_location,
+                total=3,
+                max_history=-1,
+            )
+            is None
+        )
+
+    with ignore_file.open("r", encoding="utf-8") as file:
+        post_len = len(json.load(file))
+    assert post_len - 1 == pre_len
