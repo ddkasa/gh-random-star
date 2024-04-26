eba8957690bd40475e681aebbe2f01713b2c8bcb
David Kasakaitis <davidkasakaitis@proton.me>
2024-04-26 18:52:26 +0100

    test: cli integration

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -14,3 +14,16 @@ def test_cli(set_user_settings):
     out, _ = process.communicate(input=b"1")
 
     assert process.returncode == 0
+
+
+@pytest.mark.integration
+def test_cli_integration(set_user_settings):
+    process = subprocess.Popen(
+        ["gh-star", "-a", set_user_settings[0], "--total", "3", "-r"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    out, _ = process.communicate(input=b"1")
+
+    assert process.returncode == 0
