a720ed0c6cf962c82c893bb1be44e5bff4d5c8d0
David Kasakaitis <davidkasakaitis@proton.me>
2024-04-26 18:52:15 +0100

    test: fix api integration

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,18 +1,19 @@
 import pytest
-
-from github_random_star.__main__ import retrieve_cache
+from github_random_star.api import GithubAPI
 
 
 @pytest.mark.integration
-def test_integration(set_user_settings, load_starred_items):
+def test_integration(
+    set_user_settings,
+    load_starred_items,
+    cache_location,
+):
     user, max_results, _ = set_user_settings
-    assert (
-        len(
-            retrieve_cache(
-                user,
-                refresh=True,
-                max_results=max_results,
-            )
-        )
-        == max_results
+    gh_api = GithubAPI(
+        account=user,
+        cache_location=cache_location,
+        refresh=True,
+        max_results=max_results,
     )
+
+    assert len(gh_api.collect_starred_items()) == max_results
