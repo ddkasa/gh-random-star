7c98f1aad6cf3b914f614533cad74c2ca35eeaff
David Kasakaitis <davidkasakaitis@proton.me>
2024-06-12 12:18:53 +0100

    feat: gh cli extension

---
+++ github_random_star/extension.py
@@ -0,0 +1,29 @@
+import sys
+
+from cleo.application import Application
+from cleo.io.inputs.string_input import StringInput
+
+from github_random_star.__main__ import RandomStarCommand
+from github_random_star.version import __version__
+
+
+def run(args=None):
+    app = Application(
+        name="Github Random Star",
+        version=__version__,
+    )
+    cmd = RandomStarCommand()
+    app.add(cmd)
+
+    try:
+        if args:
+            input = StringInput(args)
+            input.set_stream(sys.stdin)
+            result = app.run(input, sys.stdout, sys.stderr)
+        else:
+            result = app.run()
+    except (EOFError, KeyboardInterrupt):
+        print("User Ended Session!")
+        result = 0
+
+    return result
