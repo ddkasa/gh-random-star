42efa642687520fc18fd59bceffadfa2ab8d29d0
David Kasakaitis <davidkasakaitis@proton.me>
2024-06-12 12:19:14 +0100

    refactor: fire to cleo

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,12 @@
-from pathlib import Path
-
-import pytest
-import mock
 import builtins
+from pathlib import Path
+import json
 
-from github_random_star.__main__ import item_selection
+import mock
+import pytest
+from cleo.application import Application
+from cleo.testers.command_tester import CommandTester
+from github_random_star.__main__ import RandomStarCommand
 
 
 @pytest.mark.unit
@@ -14,32 +16,34 @@ def test_cache_path(cache_location):
 
 
 @pytest.mark.unit
-def test_random_selection(set_seed, cache_location, get_data):
+def test_random_selection(set_seed, cache_location, get_data, set_user_settings):
+    app = Application()
+    cmd = RandomStarCommand()
+    app.add(cmd)
+
+    command = app.find("star")
+    tester = CommandTester(command)
+
     with mock.patch.object(builtins, "input", lambda _: 2):
-        assert isinstance(
-            item_selection(
-                get_data,
-                cache_location,
-                total=3,
-                max_history=-1,
-            ),
-            dict,
-        )
+        assert tester.execute(set_user_settings[0]) == 0
 
 
 @pytest.mark.unit
 def test_ignore_file(set_seed, cache_location, set_user_settings, get_data):
     user, max_results, _ = set_user_settings
     pre_len = len(get_data["ignore"])
+    app = Application()
+    cmd = RandomStarCommand()
+    app.add(cmd)
+
+    command = app.find("star")
+    tester = CommandTester(command)
 
     with mock.patch.object(builtins, "input", lambda _: 2.1):
-        sel = item_selection(
-            get_data,
-            cache_location,
-            total=3,
-            max_history=-1,
-        )
+        assert tester.execute(f"{user} --max_results {max_results}") == 0
 
-    post_len = len(sel["ignore"])
+    cl = cache_location / f"{user}_cache.json"
+    with cl.open() as f:
+        post_len = len(json.load(f)["ignore"])
 
     assert post_len - 1 == pre_len
